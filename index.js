b24d4f8f4e2be8b2b8899da12dfa131ac07caf86
remnovmikita <remnovmikita@gmail.com>
2026-02-21 17:28:16 +0000

    Deploying to gh-pages from @ remnovmikita/LastBlendetJs@51d012c2b438e506827bc578945d0ab946b6b7fe 🚀

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import"./assets/styles-JE8YjOlG.js";import{a as e}from"./assets/vendor-BJ9gahTP.js";const s="https://dummyjson.com/products/category-list",a=async()=>{const{data:t}=await e.get(s);return["All",...t]},c={categories:document.querySelector(".categories")},n=t=>t.map(o=>`
+import"./assets/styles-JE8YjOlG.js";import{a}from"./assets/vendor-BJ9gahTP.js";a.defaults.baseURL="https://dummyjson.com/products";const u=async()=>{const{data:t}=await a.get("/category-list");return["All",...t]},d=async(t=1)=>{const s=`?limit=12&skip=${(t-1)*12}`,{data:c}=await a.get(s);return c.products},o={categories:document.querySelector(".categories"),products:document.querySelector(".products")},l=t=>t.map(s=>`
         <li class="categories__item">
-        <button class="categories__btn" type="button">${o}</button>
+        <button class="categories__btn" type="button">${s}</button>
         </li>
-        `).join("");r();async function r(){const t=await a();c.categories.innerHTML=n(t)}
+        `).join(""),_=t=>t.map(({id:s,title:c,description:r,category:e,price:n,thumbnail:i,brand:p})=>`<li class="products__item" data-id="${s}">
+    <img class="products__image" src="${i}" alt="${r}"/>
+    <p class="products__title">${c}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand: ${p}</span></p>
+    <p class="products__category">Category: ${e}</p>
+    <p class="products__price">Price: ${n}</p>
+ </li>
+`);g();async function g(){const t=await u();o.categories.innerHTML=l(t);const s=await d();o.products.innerHTML=_(s)}
 //# sourceMappingURL=index.js.map
